27f8c9372c23525ec823e89d5c597b71bbbbcf3c
zyhuang <zyhuang@tom.com>
2018-11-08 15:13:30 -0800

    new

---
+++ src/ndsclpto_app.erl
@@ -0,0 +1,16 @@
+-module(ndsclpto_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ndsclpto_sup:start_link().
+
+stop(_State) ->
+    ok.
